2ea3cbba746d27e478f8dffa3345e8c5288af677
zkda <school@service70.ru>
2023-11-27 00:29:18 +0700

    alfa 1.4.0
    начал писать рисование блоков

--- draw_block_diagram.py
+++ draw_block_diagram.py
@@ -1,27 +1,53 @@
-from PIL import Image, ImageDraw
-
 test = [{'insaid_code': None,
-         'item': "n = int(input('РІРІРµРґРёС‚Рµ n'))",
+         'item': "n = int(input('иван лох'))",
          'type': 128},
         {'insaid_code': None, 'item': 'divider = 0', 'type': 512},
-        {'insaid_code': None, 'item': 'dividend = 0', 'type': 512},]
+        {'insaid_code': None, 'item': 'dividend = 0', 'type': 512}, ]
 
-# Создаем изображение размером 300x300 пикселей
-image = Image.new('RGB', (300, 300), color='white')
-
-# Создаем объект для рисования на изображении
-draw = ImageDraw.Draw(image)
-
-# Рисуем прямоугольник
-draw.rectangle((50, 50, 200, 200), outline='black')
-
-# Рисуем текст
-text = 'Hello, world!'
-draw.text((100, 100), text, fill='red')
-
-# Сохраняем изображение в файл
-image.save('image.png')
+from PIL import Image, ImageDraw, ImageFont
 
 
-def painter(block_info):
-    pass
+class PaintBlock:
+    def tuple_rect(self, item, font='arial.ttf', size=20):
+        font = ImageFont.truetype(font, size)
+        text_size = font.font.getsize(item)[0]
+        imge_size = (text_size[0] + 20, text_size[1] + 20)
+        rect_size = (2, 2, text_size[0] + 20 - 3, text_size[1] + 20 - 3)
+
+        img = Image.new("RGB", imge_size, (0, 0, 0))
+        draw = ImageDraw.Draw(img)
+
+        draw.rectangle(rect_size, fill=(255, 255, 255))
+        draw.text((10, 10), item, (0, 0, 0), font=font)
+
+        # img.show('charimg')
+
+        return img
+
+    def tuple_user_interaction(self, item, font='arial.ttf', size=20):
+        font = ImageFont.truetype(font, size)
+        text_width, text_height = font.font.getsize(item)[0]
+        imge_size = (text_width + 40, text_height + 30)
+        dots = [
+            (20, 0,),
+            (text_width + 38, 0,),
+            (0, text_height + 28,),
+            (text_width + 20, text_height + 28,),
+        ]
+
+        img = Image.new("RGB", imge_size, (255, 255, 255))
+        draw = ImageDraw.Draw(img)
+
+        draw.line((*dots[0], *dots[1]), fill=(0, 0, 0), width=2)
+        draw.line((*dots[1], *dots[3]), fill=(0, 0, 0), width=2)
+        draw.line((*dots[0], *dots[2]), fill=(0, 0, 0), width=2)
+        draw.line((*dots[2], *dots[3]), fill=(0, 0, 0), width=2)
+
+        draw.text((20, 10), item, (0, 0, 0), font=font)
+
+        return img
+
+
+PaintBlock().tuple_user_interaction(test[0]['item']).show('charimg')
+
+# img.save('rectangle_with_text.png')
