6768536bc9a9b38af333449ef129f280ef0f9056
zkda <school@service70.ru>
2023-11-27 23:19:43 +0700

    alfa 1.4.1
    начал писать рисование блоков

--- draw_block_diagram.py
+++ draw_block_diagram.py
@@ -18,7 +18,7 @@ class PaintBlock:
         draw = ImageDraw.Draw(img)
 
         draw.rectangle(rect_size, fill=(255, 255, 255))
-        draw.text((10, 10), item, (0, 0, 0), font=font)
+        draw.text((10, 10), item, (0, 0, 0), font=font, )
 
         # img.show('charimg')
 
@@ -28,26 +28,112 @@ class PaintBlock:
         font = ImageFont.truetype(font, size)
         text_width, text_height = font.font.getsize(item)[0]
         imge_size = (text_width + 40, text_height + 30)
-        dots = [
-            (20, 0,),
-            (text_width + 38, 0,),
-            (0, text_height + 28,),
-            (text_width + 20, text_height + 28,),
-        ]
+        dots = {
+            'A': (20, 0,),
+            'B': (text_width + 38, 0,),
+            'C': (0, text_height + 28,),
+            'D': (text_width + 20, text_height + 28,),
+        }
 
         img = Image.new("RGB", imge_size, (255, 255, 255))
         draw = ImageDraw.Draw(img)
 
-        draw.line((*dots[0], *dots[1]), fill=(0, 0, 0), width=2)
-        draw.line((*dots[1], *dots[3]), fill=(0, 0, 0), width=2)
-        draw.line((*dots[0], *dots[2]), fill=(0, 0, 0), width=2)
-        draw.line((*dots[2], *dots[3]), fill=(0, 0, 0), width=2)
+        draw.line((*dots['A'], *dots['B']), fill=(0, 0, 0), width=2)
+        draw.line((*dots['A'], *dots['C']), fill=(0, 0, 0), width=2)
+        draw.line((*dots['B'], *dots['D']), fill=(0, 0, 0), width=2)
+        draw.line((*dots['C'], *dots['D']), fill=(0, 0, 0), width=2)
 
         draw.text((20, 10), item, (0, 0, 0), font=font)
 
         return img
 
+    def tuple_ends(self, item, font='arial.ttf', size=20):
+        font = ImageFont.truetype(font, size)
+        text_width, text_height = font.font.getsize(item)[0]
+        imge_size = (text_width + 40, text_height + 30)
+        dots = {
+            'A': (20, 0,),
+            'B': (text_width + 20, 0,),
+            'C': (20, text_height + 28,),
+            'D': (text_width + 20, text_height + 28,),
+        }
 
-PaintBlock().tuple_user_interaction(test[0]['item']).show('charimg')
+        img = Image.new("RGB", imge_size, (255, 255, 255))
+        draw = ImageDraw.Draw(img)
+
+        draw.line((*dots['A'], *dots['B']), fill=(0, 0, 0), width=2)
+        draw.line((*dots['C'], *dots['D']), fill=(0, 0, 0), width=2)
+
+        draw.arc(
+            xy=(0, 0, 40, text_height + 28),
+            start=90, end=270,
+            fill=(0, 0, 0),
+            width=2
+        )
+        draw.arc(
+            xy=(text_width, 0, text_width + 39, text_height + 28),
+            start=270, end=90,
+            fill=(0, 0, 0),
+            width=2
+        )
+
+        draw.text((20, 10), item, (0, 0, 0), font=font)
+
+        return img
+
+    def tuple_for(self, item, font='arial.ttf', size=20):
+        font = ImageFont.truetype(font, size)
+        text_width, text_height = font.font.getsize(item)[0]
+        imge_size = (text_width + 40, text_height + 30)
+        dots = {
+            'A': (20, 0,),
+            'B': (text_width + 20, 0,),
+            'AC': (0, (text_height + 28) // 2,),
+            'C': (20, text_height + 28,),
+            'D': (text_width + 20, text_height + 28,),
+            'BD': (text_width + 40, (text_height + 28) // 2,),
+        }
+
+        img = Image.new("RGB", imge_size, (255, 255, 255))
+        draw = ImageDraw.Draw(img)
+
+        draw.line((*dots['A'], *dots['B']), fill=(0, 0, 0), width=2)
+        draw.line((*dots['C'], *dots['D']), fill=(0, 0, 0), width=2)
+
+        draw.line((*dots['A'], *dots['AC']), fill=(0, 0, 0), width=2)
+        draw.line((*dots['C'], *dots['AC']), fill=(0, 0, 0), width=2)
+        draw.line((*dots['B'], *dots['BD']), fill=(0, 0, 0), width=2)
+        draw.line((*dots['D'], *dots['BD']), fill=(0, 0, 0), width=2)
+
+        draw.text((20, 10), item, (0, 0, 0), font=font)
+
+        return img
+
+    def tuple_if(self, item, font='arial.ttf', size=20):
+        font = ImageFont.truetype(font, size)
+        text_width, text_height = font.font.getsize(item)[0]
+        imge_size = (text_width * 2, int(text_height * 2.6))
+        dots = {
+            'A': ((text_width * 2) // 2, 1,),
+            'B': (0, imge_size[1] // 2,),
+            'C': ((text_width * 2) // 2, int(text_height * 2.6) - 2,),
+            'D': (text_width * 2, imge_size[1] // 2,),
+        }
+
+        img = Image.new("RGB", imge_size, (255, 255, 255))
+        draw = ImageDraw.Draw(img)
+
+        draw.line((*dots['A'], *dots['B']), fill=(0, 0, 0), width=2)
+        draw.line((*dots['B'], *dots['C']), fill=(0, 0, 0), width=2)
+        draw.line((*dots['C'], *dots['D']), fill=(0, 0, 0), width=2)
+        draw.line((*dots['D'], *dots['A']), fill=(0, 0, 0), width=2)
+
+        draw.text((imge_size[0] // 2 - text_width // 2, imge_size[1] // 2 - text_height // 2)
+                  , item, (0, 0, 0), font=font)
+
+        return img
+
+
+PaintBlock().tuple_if("'eui5toyui6 h[t0r69p'e5gkobhwegho'iafjcuyp9or5;eis").show('charimg')
 
 # img.save('rectangle_with_text.png')
