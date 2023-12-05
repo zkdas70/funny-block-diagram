97136265d1a52083933e4b125d252f77b49572d6
zkda <school@service70.ru>
2023-12-05 17:15:44 +0700

    beta 1.0.0

--- creating_pictures_in_folder_not_GUI.py
+++ creating_pictures_in_folder_not_GUI.py
@@ -9,25 +9,30 @@ FOLDER_NAMES = {
     'output': 'output', # имя папки для вывода
 }
 
-if os.path.exists(f'{FOLDER_PATH}/{FOLDER_NAMES['output']}'):
-    shutil.rmtree(f'{FOLDER_PATH}/{FOLDER_NAMES['output']}')
+if __name__ == '__main__':
+    if os.path.exists(f'{FOLDER_PATH}/{FOLDER_NAMES['output']}'):
+        shutil.rmtree(f'{FOLDER_PATH}/{FOLDER_NAMES['output']}')
 
-for name in FOLDER_NAMES.values():
-    path = f'{FOLDER_PATH}/{name}'
-    if not os.path.exists(path):
-        os.makedirs(path)
+    for name in FOLDER_NAMES.values():
+        path = f'{FOLDER_PATH}/{name}'
+        if not os.path.exists(path):
+            os.makedirs(path)
 
-files = os.listdir(f'{FOLDER_PATH}/{FOLDER_NAMES['input']}')
-for file in files:
-    with open(f'{FOLDER_PATH}/{FOLDER_NAMES['input']}/{file}') as f:
-        file_code = f.readlines()
-    decodet_file_code = block.Block().decoder(file_code)
+    files = os.listdir(f'{FOLDER_PATH}/{FOLDER_NAMES['input']}')
+    for file in files:
 
-    img = draw_block_diagram.PaintBlock().type_defld_code(decodet_file_code[0])
-    img.save(f'{FOLDER_PATH}/{FOLDER_NAMES['output']}/{file.split('.')[0]}_cobe.png')
+        with open(f'{FOLDER_PATH}/{FOLDER_NAMES['input']}/{file}') as f:
+            file_code = f.readlines()
+        os.remove(f'{FOLDER_PATH}/{FOLDER_NAMES['input']}/{file}')
+        decodet_file_code = block.Block().decoder(file_code)
 
-    for i in range(len(decodet_file_code[1])):
-        img = draw_block_diagram.draw_block_diagram([decodet_file_code[1][i]])
-        img.save(f'{FOLDER_PATH}/{FOLDER_NAMES['output']}/{file.split('.')[0]}_{decodet_file_code[1][i]['item']}.png')
+        file_name = file.split('.')[0]
+        os.makedirs(f'{FOLDER_PATH}/{FOLDER_NAMES['output']}/{file_name}')
 
+        img = draw_block_diagram.PaintBlock().type_defld_code(decodet_file_code[0])
+        img.save(f'{FOLDER_PATH}/{FOLDER_NAMES['output']}/{file_name}/cobe.png')
+
+        for i in range(len(decodet_file_code[1])):
+            img = draw_block_diagram.draw_block_diagram([decodet_file_code[1][i]])
+            img.save(f'{FOLDER_PATH}/{FOLDER_NAMES['output']}/{file_name}/{decodet_file_code[1][i]['item']}.png')
 
